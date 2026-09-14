40a31d2edc56438c8e53719cea9ceca50e7adfe1
Stefano <stefano.marinaro2015@gmail.com>
2026-09-14 11:32:03 +0200

    Aggiornato

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+    return (
+        <section>
+            <h1>Stefano Marinaro</h1>
+            <p>Web developer, futuro developer di videogiochi</p>
+            <p>Studente</p>
+        </section>
+    )
+}
+
+export default Hero
---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+function Projects() {
+    return (
+        <section>
+            <h2>Progetti</h2>
+
+            <div>
+                <h3>GoCare</h3>
+                <p>GoCare connette assistiti, caregiver e associazioni per la gestione di trasporti assistiti. Ho curato il frontend React Native con Expo Router, gestendo routing basato su ruolo utente e componenti UI riutilizzabili, in un team di quattro sviluppatori con backend ASP.NET Core ed Entity Framework.</p>
+                <a href="https://github.com/Stefano-Marinaro/project_workSGEF" target="_blank" rel="noopener noreferrer">Vedi su GitHub</a>
+            </div>
+            <div>
+                <h3>Platform game</h3>
+                <p>un gioco sviluppato con Unity</p>
+                <a href="https://github.com/Stefano-Marinaro/Esercizio1" target="_blank" rel="noopener noreferrer">Vedi su GitHub</a>
+            </div>
+        </section>
+    )
+}
+
+export default Projects
